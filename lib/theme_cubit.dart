f3100e7bd5062323cc90fa8b667c58fff27841ec
Jeffrey Lee <jkclee123@gmail.com>
2022-02-26 03:05:18 +0800

    get platform brightness from widgetsBinding

--- lib/theme_cubit.dart
+++ lib/theme_cubit.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/scheduler.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class ThemeCubit extends Cubit<ThemeData> {
   ThemeCubit()
-      : super(SchedulerBinding.instance!.window.platformBrightness ==
+      : super(WidgetsBinding.instance!.platformDispatcher.platformBrightness ==
                 Brightness.light
             ? _lightTheme
             : _darkTheme);
