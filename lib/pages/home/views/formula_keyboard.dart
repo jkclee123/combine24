81d3b42884b0e5c778b1938461439247ac397f41
Jeffrey Lee <jkclee123@gmail.com>
2022-03-04 03:34:04 +0800

    bug fix, treat 10 as 1 char

--- lib/pages/home/views/formula_keyboard.dart
+++ lib/pages/home/views/formula_keyboard.dart
@@ -1,5 +1,4 @@
 import 'dart:math';
-import 'dart:ui';
 
 import 'package:combine24/config/const.dart';
 import 'package:combine24/utils/op_util.dart';
@@ -61,7 +60,9 @@ class _FormulaKeyboardState extends State<FormulaKeyboard> {
     bool isInBracketCopy = false;
     int lenFromBracketCopy = 0;
     List<bool> availCardCopy = [true, true, true, true];
+    ans = ans.replaceAll("10", "T");
     for (String char in ans.split(Const.emptyString)) {
+      char = char.replaceAll("T", "10");
       if (OpUtil.isOpenBracket(char)) {
         isInBracketCopy = true;
         lenFromBracketCopy = 0;
