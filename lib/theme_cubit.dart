47c44f63416738253599e55d7791a976a82efdd3
Jeffrey Lee <jkclee123@gmail.com>
2025-10-07 22:40:02 +0800

    change background color

--- lib/theme_cubit.dart
+++ lib/theme_cubit.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:web/web.dart' as web;
+import 'dart:js_interop';
 
 class ThemeCubit extends Cubit<ThemeData> {
   ThemeCubit()
       : super(WidgetsBinding.instance.platformDispatcher.platformBrightness ==
                 Brightness.light
             ? _lightTheme
-            : _darkTheme);
+            : _darkTheme) {
+    // Notify initial theme state to web page
+    _notifyWebThemeChange(state.brightness);
+  }
 
   static final _lightTheme = ThemeData.light().copyWith(
     appBarTheme: const AppBarTheme().copyWith(
@@ -31,6 +36,25 @@ class ThemeCubit extends Cubit<ThemeData> {
   );
 
   void toggleTheme() {
-    emit(state.brightness == Brightness.light ? _darkTheme : _lightTheme);
+    final newTheme = state.brightness == Brightness.light ? _darkTheme : _lightTheme;
+    emit(newTheme);
+
+    // Notify web page of theme change for consistent browser theming
+    _notifyWebThemeChange(newTheme.brightness);
+  }
+
+  void _notifyWebThemeChange(Brightness brightness) {
+    try {
+      // Use JavaScript interop to notify the web page of theme changes
+      web.window.postMessage(
+        {
+          'type': 'theme-change',
+          'theme': brightness == Brightness.dark ? 'dark' : 'light'
+        }.jsify(),
+        '*'.toJS,
+      );
+    } catch (e) {
+      // Silently fail if not running in web context
+    }
   }
 }
